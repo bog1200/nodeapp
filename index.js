ccb0cacf82593076345947085907db0650417322
Bog12555 <zaruri.20@gmail.com>
2019-05-24 13:47:10 +0300

    Update

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ var token2=" ";
 //
 var fs = require('fs');
 var readline = require('readline');
-var google = require('googleapis');
+var {google} = require('googleapis');
 var googleAuth = require('google-auth-library');
+let privatekey = require("./privatekey.json");
+
 var check=-1;
 var dd=" ";
 var d=-1;
@@ -32,150 +34,26 @@ var p=-1;
 var t=-1;
 var a=-1;
 var tw=-1;
+var sa=-1;
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/google-apis-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'google-apis-nodejs-quickstart.json';
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the YouTube API.
-  //See full code sample for authorize() function code.
-authorize(JSON.parse(content));
-
+let jwtClient = new google.auth.JWT(
+       privatekey.client_email,
+       null,
+       privatekey.private_key,
+       ['https://www.googleapis.com/auth/youtube.readonly']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+ if (err) {
+   console.log(err);
+   return;
+ } else {
+	dd=tokens.access_token;
+   console.log("Successfully connected!");
+ }
 });
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
- var g_secret =" ";
- var g_id=" ";
- var g_reftoken=" ";
-function authorize(credentials, requestData, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  g_secret=clientSecret;
-  var clientId = credentials.installed.client_id;
-  g_id=clientId;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, requestData, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client, requestData);
-    }
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, requestData, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      //storeToken(token);
-	 // token2 = JSON.stringify(token.access_token);
-	//  g_reftoken = JSON.stringify(token.refresh_token);
-	  token2= token.access_token;
-	  g_reftoken= token.refresh_token;
-	  //console.log("tt ",tt);
-	  setTimeout(load,15000);
-      //callback(oauth2Client, requestData);
-    });
-  });
-}
-/**
- * Remove parameters that do not have values.
- *
- * @param {Object} params A list of key-value pairs representing request
- *                        parameters and their values.
- * @return {Object} The params object minus parameters with no values set.
- */
-function removeEmptyParameters(params) {
-  for (var p in params) {
-    if (!params[p] || params[p] == 'undefined') {
-      delete params[p];
-    }
-  }
-  return params;
-}
-
-/**
- * Create a JSON object, representing an API resource, from a list of
- * properties and their values.
- *
- * @param {Object} properties A list of key-value pairs representing resource
- *                            properties and their values.
- * @return {Object} A JSON object. The function nests properties based on
- *                  periods (.) in property names.
- */
-function createResource(properties) {
-  var resource = {};
-  var normalizedProps = properties;
-  for (var p in properties) {
-    var value = properties[p];
-    if (p && p.substr(-2, 2) == '[]') {
-      var adjustedName = p.replace('[]', '');
-      if (value) {
-        normalizedProps[adjustedName] = value.split(',');
-      }
-      delete normalizedProps[p];
-    }
-  }
-  for (var p in normalizedProps) {
-    // Leave properties that don't have values out of inserted resource.
-    if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
-      var propArray = p.split('.');
-      var ref = resource;
-      for (var pa = 0; pa < propArray.length; pa++) {
-        var key = propArray[pa];
-        if (pa == propArray.length - 1) {
-          ref[key] = normalizedProps[p];
-        } else {
-          ref = ref[key] = ref[key] || {};
-        }
-      }
-    };
-  }
-  return resource;
-  console.log(resource);
-}
-
 
 
 
@@ -234,31 +112,33 @@ function callback_t(error2, responset, bodyt) {
 else {/*console.error("Error:",responset.statuscode);*/}
 	setTimeout(lol, 1000);
 }
+var url_t="";
+var options_s = {
+	url: url_t,};
+function callback_s(error4, responses, bodys) {
+	//console.error("ER ",options_p)
+  if (!error4 && responses.statusCode == 200) {
+  const infos = JSON.parse(bodys);
+  // const subs = JSON.parse(stat);
+  var ssub= infos.items[0].statistics.subscriberCount
+sa=parseInt(ssub, 10);}}
+  
+	request(options_a, callback_a); 
 function callback_r(error3, responser, bodyr) { token2=bodyr.access_token; console.log("Renew",bodyr);}
 var pvt='lol';
 var pvt2='lol';
-var options_r = {
-	url: 'https://www.googleapis.com/oauth2/v4/token',
-	method: 'POST',
-	headers: 'Content-Type: application/x-www-form-urlencoded',
-	body: {'client_id': `${g_id}`,
-	'client_secret':`${g_secret}`,
-	'refresh_token':'1/bA-Jd5ioYH7uefR26K9rhlIqiKeZa_g1CqIQn4trY4E',
-	'&grant_type':'refresh_token',}
-	};
+
 	
-load();
-function load(){
-	console.error("Token: ",token2);
-	console.error("Refresh Token: ",g_reftoken);
-	dd=token2.substr(0,129);
+setTimeout(load,5000);
+ function load(){
+	//dd=token2.substr(0,129);
 	//var uri="https://www.googleapis.com/youtube/v3/channels?part=statistics&access_token="+`${dd}`+"&id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&fields=items/statistics/subscriberCount"
 	var uri="https://www.googleapis.com/youtube/v3/channels?id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&part=statistics&fields=items/statistics/subscriberCount&access_token="+`${dd}`;
 	var uri2="https://www.googleapis.com/youtube/v3/channels?id=UCq-Fj5jknLsUf-MWSy4_brA&part=statistics&fields=items/statistics/subscriberCount&access_token="+`${dd}`;
 	var uri3="https://www.googleapis.com/youtube/v3/channels?id=UC73wv11MF_jm6v7iz3kuO8Q&part=statistics&fields=items/statistics/subscriberCount&access_token="+`${dd}`;
 	
 	//var refresh_body='client_id:'+`${g_id}`+'&client_secret:'+`${g_secret}`+'&refresh_token:'+`${g_reftoken}`+'&grant_type:refresh_token'
-	console.error("T2: ",uri);
+	//console.error("T2: ",uri);
 	tmp=`"Authorization": "Bearer ${dd}"`;
 options_p.url=uri;
 options_t.url=uri2;
@@ -275,13 +155,12 @@ function lol(){
 d=p-t;
 if (d<0) {d=t-p;tw=1;}
 else tw=0;
-
+if (d>1000000 || d<-1000000) d=(d/1000000).toFixed(3)+'M';
 pvt='PewDiePie e in fata T-Series cu '+d+' abonati';
 if (tw==1) pvt='T-Series e in fata PewDiePie cu '+d+' abonati'
 if (d == -1) {console.log('!!!'); pvt="Data not avaliable!"}
 pew="PewDiePie: "+(p/1000000).toFixed(3)+'M';
 tsr="T-Series: "+(t/1000000).toFixed(3)+'M';
-if (d>1000000 || d<-1000000) d=(d/1000000).toFixed(3)+'M';
 if (tw==0)pvt2='Win: P+'+d;
 else pvt2="Win: T+"+d;
 //
@@ -294,10 +173,49 @@ client.on('message', msg => {
 	  //console.log(`Command .pvt triggered by ${msg.author.username}#${msg.author.discriminator} (#${msg.channel.name} on ${msg.guild.name}) at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 	  console.log(`Bot triggered with "${msg.content}" by ${msg.author.username}#${msg.author.discriminator} (#${msg.channel.name} on ${msg.guild.name}) at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
       request(options_p, callback_p);
+	  const Embed = new Discord.RichEmbed()
+	.setColor('#800080')
+	.setTitle('PewDiePie vs T-Series')
+	.setDescription(pvt)
+	.setTimestamp()
+	.setFooter(`Message requested by ${msg.author.username}#${msg.author.discriminator}` );
 	//  channel.setName('not_general')
-	  msg.channel.send(pvt);
+	  msg.channel.send(Embed);
   }}
   )
+
+  client.on('message', msg => {
+  if (msg.content === '.time') {
+	  //console.log(`Command .pvt triggered by ${msg.author.username}#${msg.author.discriminator} (#${msg.channel.name} on ${msg.guild.name}) at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+	  console.log(`Bot triggered with "${msg.content}" by ${msg.author.username}#${msg.author.discriminator} (#${msg.channel.name} on ${msg.guild.name}) at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+	const Embed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Time')
+	.setDescription(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+	.setTimestamp()
+	.setFooter(`Message requested by ${msg.author.username}#${msg.author.discriminator}` );
+	  msg.channel.send(Embed);
+	  msg.delete(1);
+  }}
+  )
+ client.on('message', msg => {
+  if (msg.content.substr(0,5) === '.subs') {
+	  console.log(`Bot triggered with "${msg.content}" by ${msg.author.username}#${msg.author.discriminator} (#${msg.channel.name} on ${msg.guild.name}) at ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+	  uri_t="https://www.googleapis.com/youtube/v3/channels?id="+`${msg.content.substr(6,31)}`+"&part=statistics&fields=items/statistics/subscriberCount&access_token="+`${dd}`;
+	request(options_s, callback_s);
+	console.error("T3: ",uri_t);
+	const Embed = new Discord.RichEmbed()
+	.setColor('#000fff')
+	.setTitle('Subscriber Count')
+  .addField("Channel ID: ",`${msg.content.substr(6,31)}`)
+  .addField("Subscribers: ",`${sa}`)
+	.setTimestamp()
+	.setFooter(`Message requested by ${msg.author.username}#${msg.author.discriminator}` );
+	  msg.channel.send(Embed);
+	  msg.delete(1);
+  }}
+  )  
+  //dd=token2.substr(0,129);
 function repeat(){
 //console.log('Diferenta:', d);
 ///lient.channels.find("id","545918988409110548").setName(pvt2);
