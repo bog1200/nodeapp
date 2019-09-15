47a75419db9bb0fdd26a2e712464812c6a0fdf6d
bog1200 <26150024+bog1200@users.noreply.github.com>
2019-09-15 23:41:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const requestHandler = (request, response) => {
   console.log(request.url)
 if (request.url == "/"){ response.writeHead(302, { Location: '/api/' }); response.end();}; 
 if (request.url == "/api/"){response.end('Bot is online!')}
-if (request.url == "/api/time"){response.end(`${Date.now()}`;);}
+if (request.url == "/api/time"){response.end(`${Date.now()}`);}
 else {response.writeHead(404); response.end();}; 
 }
 
