057d61287b1d03e01014d40f8ef3259356bff885
Bog12555 <zaruri.20@gmail.com>
2020-05-14 10:33:49 +0300

    .code v2

--- commands/code.js
+++ commands/code.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'code',
 	description: 'Shows link to bot code',
 	execute(message, args) { 
-        const Embed=new Discord.MessageEmbed().setColor('#ff00ff').setTitle('Quote').setDescription('Click [here](https://github.com/bog12555/nodeapp) to see bot source code').setTimestamp().setFooter(`${message.author.username}#${message.author.discriminator}`);
+        const Embed=new Discord.MessageEmbed().setColor('#ff00ff').setTitle('Quote').setDescription('[Click here to see bot source code](https://github.com/bog12555/nodeapp)').setTimestamp().setFooter(`${message.author.username}#${message.author.discriminator}`);
         message.delete();
         message.channel.send(Embed);
     }
