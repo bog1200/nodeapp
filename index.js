ac14f64d694ecb17742374565c892eea07b55fa1
bog1200 <26150024+bog1200@users.noreply.github.com>
2020-09-21 23:09:55 +0300

    .pvt remake + index.js cleanup

--- index.js
+++ index.js
@@ -38,11 +38,6 @@ client.on('ready', () => {
 console.log("[Discord] API Successfully connected!");
 });
 wait(2000);
-/*client.cache.guilds.forEach(g => {
-	g.fetchInvites().then(guildInvites => {
-		invites[g.id] = guildInvites;
-	});
-});*/
 
  exports.update = ((arg1,arg2='PLAYING',arg3='null',arg4='active') => {console.log(`${arg1},${arg2},${arg3}`);
 if (arg3!=='null') {client.user.setPresence({ activity: { name: `${arg1}`,type: `${arg2}`,url:`${arg3}` }, status: `${arg4}` });}
@@ -59,11 +54,7 @@ const login = new Promise ((resolve,reject) =>
 }
 ///
 let google_token;
-let diff=-1;
-let pew_subs=-1;
-let tsr_subs=-1;
-let alm_subs=-1;
-let winn=-1;
+
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/google-apis-nodejs-quickstart.json
 let jwtClient = new google.auth.JWT(
@@ -97,33 +88,15 @@ console.log("[Google] API Key refreshed!");
 });
 }
 
-function setUrl(channel_id,type)
-{
-if (type==0)return "https://www.googleapis.com/youtube/v3/channels?id="+`${channel_id}`+"&part=statistics&fields=items/statistics/subscriberCount&access_token="+`${google_token}`;
-else return  "https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&maxResults=1&q="+`${channel_id}`+"&access_token="+`${google_token}`;
-}
-
-
 let axios = require('axios');
-const { count } = require('console');
-let pvt,pvt2,pew,tsr;
-let cov_str;
-let c_out;
-let c_api=true;
-let alm_msg;
+let cov_str, c_out, alm_msg, alm_subs=-1; c_api=true;
 function update(){
-
 	axios.all([
-	  axios.get(setUrl("UC-lHJZR3Gqxm24_Vd_AJ5Yw",0)),
-	  axios.get(setUrl("UCq-Fj5jknLsUf-MWSy4_brA",0)),
-	  axios.get(setUrl("UC73wv11MF_jm6v7iz3kuO8Q",0)),
-	  //axios.get('https://api.covid19api.com/country/ro')
+	  axios.get(`https://www.googleapis.com/youtube/v3/channels?id=UC73wv11MF_jm6v7iz3kuO8Q&part=statistics&fields=items/statistics/subscriberCount&access_token=${google_token}`),
 	  axios.get('https://covid19-api.org/api/timeline/ro')
-	]).then(axios.spread((response1, response2, response3, response4) => {
-	  pew_subs=response1.data.items[0].statistics.subscriberCount;
-	  tsr_subs=response2.data.items[0].statistics.subscriberCount;
-	  alm_subs=response3.data.items[0].statistics.subscriberCount;
-	  c_out=response4.data;
+	]).then(axios.spread((response1, response2) => {
+	  alm_subs=response1.data.items[0].statistics.subscriberCount;
+	  c_out=response2.data;
 	})).catch(error => {
 		console.error(error);
 	  refreshKey();
@@ -132,13 +105,10 @@ function update(){
 	setTimeout(lol,5000);
 	}
 	let cdf=0;
-	let cnr=0;
 	function lol(){
 		try{
 		alm_msg="Abonati: "+`${alm_subs}`;
-		console.log("Pew: "+`${pew_subs}`);
-		console.log("Alm: "+`${alm_subs}`);
-		console.log("Tsr: "+`${tsr_subs}`);
+		//console.log("Alm: "+`${alm_subs}`);
 		//console.log(c_out[0]['cases']);
 		
 		let i=0;
@@ -157,17 +127,7 @@ function update(){
 		cov_str="Cazuri: -1"
 		c_api=false;
 	}
-	diff=pew_subs-tsr_subs;
-	if (diff<0) {diff=tsr_subs-pew_subs;winn=1;}
-	else winn=0;
-	if (diff>1000000 || diff<-1000000) diff=(diff/1000000)+'M';
-	pvt='PewDiePie e in fata T-Series cu '+diff+' abonati';
-	if (winn==1) pvt='T-Series e in fata PewDiePie cu '+diff+' abonati'
-	if (diff == -2) {console.log('!!!'); pvt="Data not avaliable!"}
-	let pew="PewDiePie: "+(pew_subs/1000000)+'M';
-	let tsr="T-Series: "+(tsr_subs/1000000)+'M';
-	if (winn==0)pvt2='Win: P+'+diff;
-	else pvt2="Win: T+"+diff;
+	
 	setTimeout(UpdateStatus, 3000);
 	//
 	}
@@ -182,9 +142,6 @@ function update(){
 	}
 	load();
 
-	
-	
-	
 	const queue = new Map();
 	client.on('message', async message => {
 		const date = new Date;
@@ -215,20 +172,13 @@ try {
 }
 });
 
-
-
 function UpdateStatus(){
 
 	//Romail.ml
-	client.channels.fetch("545918988409110548").then(channel => channel.setName(pvt2).catch(error => console.error(error)));
-	client.channels.fetch("545918846754619392").then(channel => channel.setName(pew).catch(error => console.error(error)));
-	client.channels.fetch("545918234822574111").then(channel => channel.setName(tsr).catch(error => console.error(error)));
-	client.channels.fetch("702248585991028776").then(channel => channel.setName(cov_str));
-	client.channels.fetch("733602475436802058").then(channel => channel.setName(`Noi: ${cdf}`));
-
-
+	client.channels.fetch("702248585991028776").then(channel => channel.setName(cov_str)).catch(error => console.error(error));
+	client.channels.fetch("733602475436802058").then(channel => channel.setName(`Noi: ${cdf}`)).catch(error => console.error(error));
 	///AlmostIce
-	client.channels.fetch("700813443111977021").then(channel => channel.setName(alm_msg));
+	client.channels.fetch("700813443111977021").then(channel => channel.setName(alm_msg)).catch(error => console.error(error));
 	//client.channels.find(channel => channel.id === "693109405696262164").setName(dro);
 	
 	  setTimeout(update, 1200000);
@@ -269,5 +219,3 @@ function UpdateStatus(){
 		console.log('Goodbye!');
 		client.destroy();
 	});
-	
-		
