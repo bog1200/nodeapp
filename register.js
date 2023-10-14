5a58445c6c94e4f5c6436ef02ac896c0ef5b2120
rPi <pi@romail.app>
2023-10-14 11:47:03 +0300

    Register script gracefull exit

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
+const { exit } = require('process');
 require('dotenv').config();
 
 const commands = [];
@@ -21,15 +22,16 @@ const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_KEY);
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
- 	
-	await rest.put(
-		Routes.applicationCommands(clientId),
-		{ body: commands },
-	);
- 
-		
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+
 
 		console.log('Successfully reloaded application (/) commands.');
+		exit(0);
 	} catch (error) {
 		console.error(error);
 	}
